db02f20798eaa889807613dfe5459ad9d26ae102
KUE9514 <woodcedar.light98@gmail.com>
2020-05-07 02:50:08 +0000

    follow, unfollow

--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -1,8 +1,8 @@
 @if (Auth::id() != $user->id)
-    @if (Auth::user()->is_following($user->id)
-        {!! Form::open(['route' => ['user.unfollow', '$user_id], 'method' => 'delete']) !!}
-            {Form::submit('Unfollow', ['class' => "btn btn-danger btn-block"])
-        {!! Form:::close() !!}
+    @if (Auth::user()->is_following($user->id))
+        {!! Form::open(['route' => ['user.unfollow', $user_id], 'method' => 'delete']) !!}
+            {!! Form::submit('Unfollow', ['class' => "btn btn-danger btn-block"]) !!}
+        {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
             {!! Form::submit('Follow', ['class' => "btn btn-primary btn-block"]) !!}
